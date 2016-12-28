05dece047296fe2555324ce4d3ddb8eca15aeb62
Christian Bieniak <cbieniak@gmail.com>
2016-12-28 17:33:41 +1100

    Consolodate model classes

--- WorkoutTracker/Model/Exercise.swift
+++ WorkoutTracker/Model/Exercise.swift
@@ -1,5 +1,5 @@
 //
-//  Exercise+CoreDataProperties.swift
+//  Exercise+CoreDataClass.swift
 //  
 //
 //  Created by ChristianBieniak on 16/10/16.
@@ -9,13 +9,17 @@
 import Foundation
 import CoreData
 
+public class Exercise: NSManagedObject {
+
+}
+
 
 extension Exercise {
-
+    
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Exercise> {
         return NSFetchRequest<Exercise>(entityName: "Exercise");
     }
-
+    
     @NSManaged public var name: String?
     @NSManaged public var primaryKey: String!
     @NSManaged public var sessions: NSSet
--- WorkoutTracker/Model/Session.swift
+++ WorkoutTracker/Model/Session.swift
@@ -34,3 +34,21 @@ public class Session: NSManagedObject {
         
     }
 }
+
+extension Session {
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Session> {
+        return NSFetchRequest<Session>(entityName: "Session");
+    }
+    
+    @NSManaged public var date: NSDate?
+    @NSManaged public var reps: Double
+    @NSManaged public var time: Double
+    @NSManaged public var distance: Double
+    @NSManaged public var weight: Double
+    @NSManaged public var speed: Double
+    @NSManaged public var exercise: Exercise?
+    
+    static let attributes: [String] = ["reps", "distance", "weight", "time", "speed"]
+    
+}
